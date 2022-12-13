3568e874731246b3d8f72502ad3fb8af8f2a856b
Anastasiya <9399023@mail.ru>
2022-12-13 19:58:51 +0300

    add task 24

--- task24/Program.cs
+++ task24/Program.cs
@@ -2,3 +2,25 @@
 // 7 -> 28
 // 4 -> 10
 // 8 -> 36
+
+//Метод считывает число от пользователя
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+// метод возвращает сумму от 1 до А
+int GetSum1toNumber(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i++)
+    {
+        //sum = sum + i;
+        sum += i;
+    }
+    return sum;
+}
+
+int A = ReadInt("Введите число: ");
+int result = GetSum1toNumber(A);
+Console.WriteLine($"Сумма чисел от 1 до {A} = {result}");

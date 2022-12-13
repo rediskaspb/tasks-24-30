87fee00d22e6ad7647e61dff27a7700409a0994d
Anastasiya <9399023@mail.ru>
2022-12-13 20:27:57 +0300

    add task 26

--- task26/Program.cs
+++ task26/Program.cs
@@ -3,3 +3,20 @@
 // 78 -> 2
 // 89126 -> 5
 
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfNumber(int number)
+{
+    int count = number.ToString().Length;
+    Console.WriteLine(count);
+    return count;
+}
+
+
+int number = ReadInt("Введите число: ");
+int result = GetCountOfNumber(number);
+Console.WriteLine($"Количество цифр в числе -> {result}");

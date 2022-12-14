c3767e0337ed2ea87b6834f9eb33a295fe450940
Anastasiya <9399023@mail.ru>
2022-12-14 14:09:26 +0300

    add task 27

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,7 +3,32 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-Console.WriteLine(sum);
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfNumber(int number)
+{
+    int count = number.ToString().Length;
+    // Console.WriteLine(count);
+    return count;
+}
+
+int GetSumNumber(int number, int count)
+{
+    int sum = 0;
+    for(int i = 1; i <= count; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+        // Console.WriteLine(sum);
+    }
+    return sum;
+}
+
+int number = ReadInt("Введите число: ");
+int count = GetCountOfNumber(number);
+int result = GetSumNumber(number, count);
+Console.WriteLine($"Сумма цифр в числе = {result}");

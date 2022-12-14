cca037c93e210705bc819c30c3c9ea125d88a95c
Anastasiya <9399023@mail.ru>
2022-12-14 13:31:27 +0300

    add task 25

--- task25/Program.cs
+++ task25/Program.cs
@@ -4,3 +4,25 @@
 
 // Не использовать Math.Pow() и аналоги!
 
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AdegreeB(int A, int B)
+{
+    int degree = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        degree = degree * A;
+        // Console.WriteLine(degree);
+    }
+    return degree;
+}
+
+int A = ReadInt("Введите число A: ");
+int B = ReadInt("Введите степень B: ");
+int result = AdegreeB(A, B);
+Console.WriteLine($"{A} в степени {B} -> {result}");
+

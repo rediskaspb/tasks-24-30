b70d8b60ae07d8530449e0b1af42fd74ec3569aa
Anastasiya <9399023@mail.ru>
2022-12-14 15:07:44 +0300

    add task30 1

--- task29/Program.cs
+++ task29/Program.cs
@@ -2,4 +2,11 @@
 // Данные вводятся с консоли пользователем
 
 
+int [] array = new int[8];
+for(int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+Console.WriteLine(String.Join(", ", array));
+
 
--- task30/Program.cs
+++ task30/Program.cs
@@ -2,7 +2,7 @@
 // [1,0,1,1,0,1,0,0]
 
 int [] array = new int[8];
-for(int i = 0; i < 8; i++)
+for(int i = 0; i < array.Length; i++)
 // new Random().Next(0, 2);
 {
     array[i] = new Random().Next(0, 2);

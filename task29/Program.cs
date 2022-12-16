814d1845022d075e94164e8d9b2a45c7af1f4c4a
Anastasiya <9399023@mail.ru>
2022-12-16 14:14:17 +0300

    add task 30

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,12 +1,24 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
 // Данные вводятся с консоли пользователем
 
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numbers = ReadInt("Введите 8 чисел: ");
 
 int [] array = new int[8];
-for(int i = 0; i < 8; i++)
+for(int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(0, 2);
+    array[i] = Convert.ToInt32(Console.ReadLine());
 }
-Console.WriteLine(String.Join(", ", array));
+Console.WriteLine("--------------");
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + ", ");
+}
 
 
--- task30/Program.cs
+++ task30/Program.cs
@@ -6,6 +6,6 @@ for(int i = 0; i < array.Length; i++)
 // new Random().Next(0, 2);
 {
     array[i] = new Random().Next(0, 2);
+    // Console.Write(array[i] + ", ");
 }
 Console.WriteLine(String.Join(", ", array));
-
